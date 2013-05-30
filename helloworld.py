e49493dd872cbc602977a611ea816c89d98734e4
Gregory Kremler <gregory.kremler@gmail.com>
2013-05-29 23:06:31 -0400

    Implemented ASCII art forum with Datastore

--- helloworld.py
+++ helloworld.py
@@ -2,6 +2,8 @@ import os
 import webapp2
 import jinja2
 import validations as v
+from google.appengine.ext import db
+
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
@@ -120,10 +122,41 @@ class Welcome(BaseHandler):
             self.redirect('/signup')
 
 
+class Art(db.Model):
+    title = db.StringProperty(required=True)
+    art = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
+
+class Ascii(BaseHandler):
+    def render_ascii(self, title="", art="", error=""):
+        arts = db.GqlQuery("SELECT * FROM Art "
+                           "ORDER BY created DESC ")
+
+        self.render('ascii-form.html', title=title, art=art, error=error,
+                    arts=arts)
+
+    def get(self):
+        self.render_ascii()
+
+    def post(self):
+        title = self.request.get('title')
+        art = self.request.get('art')
+
+        if title and art:
+            a = Art(title=title, art=art)
+            a.put()
+            self.redirect('/ascii')
+        else:
+            error = "We need both a title and some artwork!"
+            self.render_ascii(title, art, error)
+
+
 app = webapp2.WSGIApplication([('/', MainPage),
                                ('/birthday', Birthday),
                                ('/thanks', Thanks),
                                ('/rot13', Rot13),
                                ('/signup', Signup),
-                               ('/welcome', Welcome)],
+                               ('/welcome', Welcome),
+                               ('/ascii', Ascii)],
                               debug=True)
